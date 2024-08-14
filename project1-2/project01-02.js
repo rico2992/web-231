21ae09b696f5b284082d4fa9b20d92aab72ec9b6
Rico <rico.isaac19@gmail.com>
2024-08-14 16:43:48 -0500

    Created project1-2 folder and added project files with dynamic service names and speeds in table rows using JavaScript.

---
+++ project1-2/project01-02.js
@@ -0,0 +1,12 @@
+ /*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Rico Isaac
+    Date: August 14, 2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate", service1Speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig";
